ca08221623b13e2821dd87fc32329d048d01645e
Yu-Chun Tung <dyj0183@byui.edu>
2021-06-30 12:17:02 -0600

    added new models & updated existing models

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,11 +11,21 @@ namespace ContosoUniversity.Models
     {
         // properties
         [DatabaseGenerated(DatabaseGeneratedOption.None)] // this attribute allows entering the PK for the course rather than having the database generate it
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
+        [Range(0, 5)]
         public int Credits { get; set; }
 
+        public int DepartmentID { get; set; }
+
+
         // navigation properties
+        public Department Department { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
 }
--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -16,6 +18,8 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; } // primary key
         public int CourseID { get; set; } // foreign key
         public int StudentID { get; set; } // foreign key
+
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         // navigation properties
